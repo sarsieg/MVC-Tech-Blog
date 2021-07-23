7f7e54a70892f1b89f19b3a3658db8b27dbe3250
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-23 16:24:48 -0500

    added functions for signup, similar to edit-post, delete-post, login, logout, etc.

---
+++ public/js/signup.js
@@ -0,0 +1,30 @@
+// login submission
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const responce = await fetch(`/api/users/login`, {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        // once our route is received properly re-direct the user to the dashboard
+        if (responce.ok) {
+            console.log('success');
+
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
