1bebca6be09380fb17fb2ff60e7574cd5036d649
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-22 22:57:13 -0500

    added function that imports the user, post and comment routes from the api folder

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+// imports the user, post, and comment routes from the api folder
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
+const { post } = require('./comment-routes.js');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
