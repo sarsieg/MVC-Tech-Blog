fcd6bcbb29cc0380fc4e9707b1ee5a6bdf966fbe
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-23 15:06:20 -0500

    added function to post stringified data to the post-routes folder of our routes and a function so that once our route is reveived properly then it calls to re-direct the user to the dashboard

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -5,4 +5,25 @@ async function newFormHandler(event) {
     // capture the values of a form considered to be add-post
     const title = document.querySelector('input[name="post-title"]').value;
     const content = document.querySelector('textarea[name="content"]').value;
-}
+
+    // post stringified data to the post-routes folder of our routes
+    const responce = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    // once our route is received properly re-direct the user to the dashboard
+    if (responce.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(responce.statusText);
+    }
+};
+
+document.querySelector('#add-post-form').addEventListener('submit', newFormHandler);
