9451843f53a3d5c0d6246d1b6b3f72d7838c52fe
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-22 23:19:06 -0500

    added function so when a user clicks on a specific post it will return that data

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -39,3 +39,41 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// when user clikcs on a specific post, return that data
+router.get('/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
