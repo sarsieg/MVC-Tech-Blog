c6cd8a4389f4e0ceed411c9045d690ed71de03f9
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-23 15:55:20 -0500

    added function to get data back from the db, same idea as update, add, login, etc. but this is for delete. as well as a event listener for a click calling it to then delete the data

---
+++ public/js/delete-post.js
@@ -0,0 +1,28 @@
+// this function is the same idea as update, add, login, etc.
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    // post stringified data to the post-routes folder of our routes
+    const responce = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+            post_id: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    // once our route is received properly re-direct the user to the dashboard
+    if (responce.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(responce.statusText);
+    }
+};
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
