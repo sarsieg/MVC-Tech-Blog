c446724ba489b8a856ec433733ed6f8edaad9bc0
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-23 00:38:50 -0500

    added id, comment_text, user_id and post_id information for comment model

--- models/Comment.js
+++ models/Comment.js
@@ -6,5 +6,39 @@ class Comment extends Model {}
 
 Comment.init({
     // sequelize figures out the id as a primary key
-    id: {}
-})
+    id: {
+        // set our value types
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        // make column auto increment
+        autoIncrement: true
+    },
+    comment_text: {
+        type: DataTypes.STRING,
+        validate: {
+            len: [3]
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'post',
+            key: 'id'
+        }
+    }
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+});
+module.exports = Comment;
