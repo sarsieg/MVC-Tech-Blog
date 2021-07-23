ed96f78762b7f6dbd876f1989fc9a3f150774d8b
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-22 23:14:27 -0500

    added function so when a post is added to find all the content and post it in reverse

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,3 +6,36 @@ const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 // import loggedIn authenticator 
 const withAuth = require('../../utils/auth');
+
+// when post is added find all its content and post it in reverse
+router.get('/', (req, res) => {
+    console.log('================');
+    Post.findAll({
+            attributes: ['id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            order: [
+                ['created_at', 'DESC']
+            ],
+            include: [{
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        })
+        .then(dbPostData => res.json(dbPostData.reverse()))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
