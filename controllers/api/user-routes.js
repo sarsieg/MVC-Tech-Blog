2a5a5965d9bf9f18395de8665e81b72de9e97dbc
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-22 23:52:27 -0500

    added function so that if the user clicks log in button to find the username and if there isn't one to return an error

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -77,3 +77,34 @@ router.post('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// when user clicks login button find username if there is no username return bad request 
+router.post('/login', (req, res) => {
+    User.findOne({
+            where: {
+                username: req.body.username
+            }
+        }).then(dbUserData => {
+            if (!dbUserData) {
+                res.status(400).json({ message: 'No user with that username' });
+                return;
+            }
+            const validPassword = dbUserData.checkPassword(req.body.password);
+
+            if (!validPassword) {
+                res.status(400).json({ message: 'Incorrect password' });
+                return;
+            }
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json({ user: dbUserData, message: 'You are now logged in' });
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
