4cb6159cf87132c7ddd7068eb6655bda0cf0f530
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-22 22:49:57 -0500

    added function that if user wants to updated a comment they must be logged in

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -46,3 +46,24 @@ router.post('/', withAuth, (req, res) => {
             })
     }
 });
+
+// if a user wants to update a comment they have to be logged in
+router.put('/:id', withAuth, (req, res) => {
+Comment.update({
+    comment_text: req.body.comment_text
+}, {
+    where: {
+        id: req.params.id
+    }
+}).then(dbCommentData => {
+    if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id' });
+        return;
+    }
+    res.json(dbCommentData);
+}).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+});
+})
