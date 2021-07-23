51c6ddfa4396377ba35f099b24c63b1380d75322
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-23 01:06:18 -0500

    added function to set up handlebars.js engine with custom helpers, secret to require sign the session id cookie, forces a session to be saved to the store and to connect the sequelize instance with express and sessions

--- server.js
+++ server.js
@@ -6,3 +6,24 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// set up handlebars.js engine instance with custom helpers
+const hbs = exphbs({ helpers });
+
+const sess = {
+    // requires to sign the session ID cookie
+    secret: 'Mary goes robblin with marigold goblins',
+    // specifies number (ms) to use when calculating expires set cookie attribue this is the session id cookie
+    cookie: { maxAge: null },
+    resave: false,
+    // forces a session to be saved to the store
+    saveUninitialized: true,
+    // the session store instance, defualts to a new memorystore instance
+    store: new SequelizeStore({
+        // connect the sequelize instance with express and sessions
+        db: sequelize
+    })
+};
