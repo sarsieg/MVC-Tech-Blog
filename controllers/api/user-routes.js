16e27b2d8881ae3f972b47ed4d7321363d861b19
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-22 23:40:24 -0500

    added function so when you click on this id you get user info but not the password

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,3 +12,46 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// when you click on this id, you get user info but not the password
+router.get('/:id', (req, res) => {
+    User.findOne({
+            attributes: { exclude: ['password'] },
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                    model: Post,
+                    attributes: [
+                        'id',
+                        'title',
+                        'content',
+                        'created_at'
+                    ]
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'created_at'],
+                    include: {
+                        model: Post,
+                        attributes: ['title']
+                    }
+                },
+                {
+                    model: Post,
+                    attributes: ['title'],
+                }
+            ]
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
