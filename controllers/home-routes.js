c19bc5a7a5d6865efa259dcf47923ab158d58e80
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-23 01:32:40 -0500

    added function to find all posts on homepage and if login button is clicked to redirect to login handlebars page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,25 +17,45 @@ router.get('/', async(req, res) => {
 router.get('/', (req, res) => {
     // read the whole table form the db with the findAll method
     Post.findAll({
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'created_at'
-        ],
-        include: [{
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
                     model: User,
                     attributes: ['username']
                 }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
+            ]
+        })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            console.log('---------------------------');
+            console.log(posts);
+            console.log('---------------------------');
+            // giving an obj w a prop called posted that is an array of obj
+            res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-})
+// if login button is clicked redirect to login handlebars page, if on route and user is logged in redirect to homepage
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
