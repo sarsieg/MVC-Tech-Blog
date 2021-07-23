821398838b4ba8e3a1de69bde2c560ec5e79f67e
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-23 01:39:49 -0500

    added function so when a user clicks on a post to view it, as well as search for a single instance, return the data and console log err if there is no post found with that specified id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -59,3 +59,53 @@ router.get('/login', (req, res) => {
     }
     res.render('login');
 });
+
+// same as our login page but signup is a login
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+// when a user clicks on a post, view it, search for a single instance
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+            // limit query to only load the post of the id selected
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'content',
+                'title',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            const post = dbPostData.get({ plain: true });
+            console.log(post);
+            res.render('single-post', { post, loggedIn: req.session.loggedIn });
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
