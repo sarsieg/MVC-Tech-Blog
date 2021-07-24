89a9b68e82f1a0d9fb4766f40dd651f4e2e6e357
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-24 12:47:43 -0500

    added code to use express' next() function so that if a user is not logged in then it calls to redirect them

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// basically, using express' next(), if a user isn't loggedIn then redirect
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
