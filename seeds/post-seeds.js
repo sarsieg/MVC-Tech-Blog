a8eb09b096e99920248e1d257a84da5f68aeb2a1
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-07-24 12:31:06 -0500

    added post-seeds.js code with post data titles, content and user_ids

---
+++ seeds/post-seeds.js
@@ -0,0 +1,27 @@
+const { Post } = require('../models');
+
+const postData = [{
+        title: "Why MVC is so important",
+        content: "MVC allows developers to maintain a true separation of concerns, devising their code between the Model layer for data, the View layer for design, and the Controller layer for application logic.",
+        user_id: 1
+    },
+    {
+        title: "Authentication vs. Authorization",
+        content: "There is a difference between authentication and authorization. Authentication means confirming your own identity, whereas authorization means being allowed access to the system.",
+        user_id: 2
+    },
+    {
+        title: "Object-Relational Mapping",
+        content: "I have really loved learned about ORMs. It's really simplified the way I create queries in SQL!",
+        user_id: 3
+    },
+    {
+        title: "Handlebars",
+        content: "Handlebars are really helpful",
+        user_id: 4
+    }
+];
+
+const seedPosts = () => Post.bulkCreate(postData);
+
+module.exports = seedPosts;
