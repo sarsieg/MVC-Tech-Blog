035d6fc3c3b949f71595458c330550bc0cdc4cc6
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-08-07 14:30:34 -0500

    fixed bug

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,8 +3,8 @@
 async function signupFormHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
     if (username && password) {
         const response = await fetch(`/api/users`, {
